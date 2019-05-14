af48e7e2963da047ca7ee51cac5b68783d62a9d2
yshafiq <yshafiq@ucsc.edu>
2019-05-13 18:03:14 -0700

    Cleaned homefragment

--- app/src/main/java/com/example/moodswings/HomeFragment.java
+++ app/src/main/java/com/example/moodswings/HomeFragment.java
@@ -1,6 +1,5 @@
 package com.example.moodswings;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,7 +7,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -43,23 +41,6 @@ public class HomeFragment extends Fragment {
         }
         moodGraph.addSeries(series);
     }
-
-    //    public static Button TodayMoodBtn;
-//
-//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        View view = inflater.inflate(R.layout.fragment_home, container,false);
-//
-//        TodayMoodBtn = (Button) view.findViewById(R.id.TodayMoodBtnID);
-//        TodayMoodBtn.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                Intent intent;
-//                intent = new Intent(HomeFragment.this, TodayMoodActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        return view;
-//    }
 
 }
 
