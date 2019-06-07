6b5d99f8dfd184a15baf4c836c991ca7db02af00
Richard Torres <rtorres513@hotmail.com>
2019-06-07 15:08:58 -0700

    Cleanup LoginActivity

--- app/src/main/java/com/example/moodswings/LoginActivity.java
+++ app/src/main/java/com/example/moodswings/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -25,7 +24,6 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
     GoogleApiClient mGoogleSignInClient;
 
-    private static final String TAG = "LoginActivity";
     private static final int RC_SIGN_IN = 9001;
     private FirebaseAuth mAuth;
 
@@ -71,7 +69,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 firebaseAuthWithGoogle(account);
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e);
             }
 
         }
@@ -79,8 +76,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -88,21 +83,15 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
                             Intent returnHome = new Intent(LoginActivity.this, MainActivity.class);
                             returnHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                             startActivity(returnHome);
                             finish();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
                         }
                     }
                 });
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult){
-        Log.d(TAG, "onConnectionFailed:"+connectionResult);
-    }
+    public void onConnectionFailed(ConnectionResult connectionResult){}
 }
